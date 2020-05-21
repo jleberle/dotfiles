0f6b196c65f1e07f6f02ab079bcea47802ea069f
Jared Eberle <me@jaredeberle.org>
2020-05-21 12:55:24 -0500

    Merge files

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -101,3 +101,52 @@ function wordfrequency() {
 
 # Open man man pages in Preview
 function pman() { man -t "$@" | open -f -a "Preview" ;}
+
+# Functions
+# (f)ind by (n)ame
+# usage: fn foo 
+# to find all files containing 'foo' in the name
+function fn() { ls **/*$1* }
+
+function wordfrequency() {
+  awk '
+     BEGIN { FS="[^a-zA-Z]+" } {
+         for (i=1; i<=NF; i++) {
+             word = tolower($i)
+             words[word]++
+         }
+     }
+     END {
+         for (w in words)
+              printf("%3d %s\n", words[w], w)
+     } ' | sort -rn
+}
+
+function pman() { man -t "$@" | open -f -a "Preview" ;}
+
+# `o` with no arguments opens the current directory, otherwise opens the given
+# location
+function o() {
+	if [ $# -eq 0 ]; then
+		open .;
+	else
+		open "$@";
+	fi;
+}
+
+# Create a new directory and enter it
+function mkd() {
+	mkdir -p "$@" && cd "$_";
+}
+
+# Change working directory to the top-most Finder window location
+function cdf() { # short for `cdfinder`
+	cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')";
+}
+
+function acp() {
+  git add .
+  git commit -S -m "$1"
+  git push
+}
+
