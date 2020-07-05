bec656f92c44a5e65010b4ee1d6735e20d807684
Jared Eberle <me@jaredeberle.org>
2020-07-05 12:11:11 -0500

    DNS fix

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -40,7 +40,7 @@ alias gsa="find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {}
 # -------------------------------------------------------------------
 alias mp3='cd ~/Music && youtube-dl -x --audio-format mp3'
 alias video='cd ~/Movies && youtube-dl -f bestvideo+bestaudio'
-alias flushdns='sudo killall -HUP mDNSResponder'
+alias flushdns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
 
 # Zippin
 alias gz='tar -zcvf'
