8fb44b058eae377e91abbab605446a16721e46e9
Jared Eberle <me@jaredeberle.org>
2020-08-09 13:33:00 -0500

    Re-enabled iTerm2 Integration

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -104,7 +104,7 @@ fi
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-# test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
 eval "$(rbenv init -)"
 
