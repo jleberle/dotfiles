a4a40d7c4b1c438f4b995d7e875245e3f6a5ee63
Jared Eberle <me@jaredeberle.org>
2021-01-05 19:10:37 -0600

    Changed zshrc for linux

--- zsh/debian-zshrc.zsh
+++ zsh/debian-zshrc.zsh
@@ -5,80 +5,41 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# ZSH configuration
-# -------------------------------------------------------------------
-export EDITOR='nvim'
-export PROJECTS=$HOME/Git # c + <tab> for autocomplete
-export ZSH=$HOME/Git/dotfiles
-export TROPY_PATH=/opt/tropy
+# Paths
+export ZSH="${HOME}/.oh-my-zsh"
+export PATH="${HOME}/.local/bin:${HOME}/.cabal/bin:$PATH"
 
-# Sourcing Aliases and Functions
-source $ZSH/zsh/debian-aliases.zsh
+# Load Antigen
+source /usr/share/zsh-antigen/antigen.zsh
 
-# Functions
-autoload -U $ZSH/zsh/functions/*(:t)
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
 
-# Colors
-eval $(dircolors "$ZSH/zsh/solarized-colors")
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# Oh-My-ZSH Configurations
+ENABLE_CORRECTION="true"
+HIST_STAMPS="yyyy-mm-dd"
 
-# Options
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-export CLICOLOR=true
-export TERM="xterm-256color"        # for common 256 color terminals (e.g. gnome-terminal)
-export TERM="screen-256color"       # for a tmux -2 session (also for screen)
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_LIST_BEEP
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt AUTO_CD
-setopt COMPLETE_IN_WORD
-setopt AUTOPUSHD        # keep history of directories
-setopt AUTO_LIST        # list ambiguous completions automatically
-setopt complete_aliases
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-# pasting with tabs doesn't perform completion
-zstyle ':completion:*' insert-tab pending
-# Vim key bindings and Vim-like line editor
-bindkey -v
-autoload -U   edit-command-line
-zle -N        edit-command-line
-bindkey -M vicmd v edit-command-line
-bindkey '^R' history-incremental-search-backward
-bindkey '^S' history-incremental-search-forward
-unsetopt nomatch
-autoload colors zsh/terminfo && colors
+# Antigen Bundles
+antigen bundle git
+antigen bundle pip
+antigen bundle cabal
+antigen bundle debian
+antigen bundle command-not-found
+antigen bundle joel-porquet/zsh-dircolors-solarized.git
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen theme romkatv/powerlevel10k
+antigen apply
 
-# Path
-# -------------------------------------------------------------------
-pathdirs=(
-  $HOME/Git/dotfiles/bin
-  $HOME/.gem/ruby/2.5.0/bin
-  $HOME/.local/bin
-  $TROPY_PATH
-)
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
 
-for dir in $pathdirs; do
-  if [ -d $dir ]; then
-    PATH=$dir:$PATH
-  fi
-done
+# Aliases
+source ${HOME}/Git/dotfiles/zsh/debian-aliases.zsh
 
-autoload -Uz compinit
-compinit
-
-# Sourcing 
-# -------------------------------------------------------------------
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-source ~/.config/powerlevel10k/powerlevel10k.zsh-theme
-source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-export GPG_TTY=$TTY
