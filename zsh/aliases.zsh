7647bb63923dcf77c3019802a44ae96d3c33c9c6
Jared Eberle <3945368+jleberle@users.noreply.github.com>
2021-08-04 12:28:02 -0500

    Moved Git aliases to gitconfig

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -40,19 +40,6 @@ else
   alias la="ls -A --color"
 fi
 
-# Git 
-# -------------------------------------------------------------------
-alias ga='git add'
-alias gc='git commit'
-alias gca='git commit -a'
-alias gcam='git commit -a -m'
-alias gcm='git commit -m'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gs='git status -sb'
-alias gl='git pull --ff-only'
-
-# -------------------------------------------------------------------
-
 function wordfrequency() {
   awk '
      BEGIN { FS="[^a-zA-Z]+" } {
