54fb46dcf0d22719b95194c823dba309a50d45a6
Jared Eberle <jared@jaredeberle.org>
2026-05-27 19:06:09 -0500

    Updated Zsh to minimize files needed

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -15,15 +15,35 @@ compinit
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-# Prompt
-eval "$(starship init zsh)"
-
 # Aliases
-alias ls="exa"
+# --------------------------------------------------------------------
+alias vim='nvim'
+alias ls="eza"
 alias cat="bat"
+
+# Editors and languages
+# -------------------------------------------------------------------
+alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias cb='xclip -i -selection clipboard'
+
+# System management
+# -------------------------------------------------------------------
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias findd="find . -type d -iname" # find a directory
+alias findf="find . -type f -iname" # find a file 
+alias fuck='sudo $(fc -ln -1)' # Redo last command with sudo
+
+alias flushdns='sudo dscacheutil -flushcache; sudo killall - HUP mDNSResponder'
+alias network='networkQuality'
+
+#Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # FZF
 source <(fzf --zsh)
 
-#Homebrew
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Prompt
+eval "$(starship init zsh)"
+
