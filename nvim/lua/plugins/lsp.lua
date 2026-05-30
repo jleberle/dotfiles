0f3ab56ada564d1844bd5fdc90907d11a74002b1
Jared Eberle <jared@jaredeberle.org>
2026-05-29 19:12:09 -0500

    Fixed error with old format

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,21 +3,19 @@ return {
 		"neovim/nvim-lspconfig",
 		ft = { "lua", "python", "sh", "bash" },
 		dependencies = { "hrsh7th/cmp-nvim-lsp" },
-
 		config = function()
-			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.pyright.setup({ capabilities = capabilities })
-			lspconfig.bashls.setup({ capabilities = capabilities })
+			vim.lsp.config("lua_ls", { capabilities = capabilities })
+			vim.lsp.config("pyright", { capabilities = capabilities })
+			vim.lsp.config("bashls", { capabilities = capabilities })
+
+			vim.lsp.enable({ "lua_ls", "pyright", "bashls" })
 		end,
 	},
-
 	{
 		"stevearc/conform.nvim",
 		event = { "BufReadPre", "BufNewFile" },
-
 		config = function()
 			require("conform").setup({
 				formatters_by_ft = {
@@ -26,9 +24,6 @@ return {
 					markdown = { "prettier" },
 				},
 			})
-
-			-- <leader>cf, not <leader>f, to avoid colliding with the
-			-- <leader>ff/fg/fb Telescope prefix (300ms timeoutlen delay).
 			vim.keymap.set("n", "<leader>cf", function()
 				require("conform").format({
 					async = true,
